5dc0d854245c6d868d7f8c4773d5ecfefbbf536a
Bogdan Kowaltchook <bogdan.kowaltchook@codeimmersives.com>
2020-02-10 23:10:16 -0500

    putthing link to script.js in the beginning of body tag doesn't overwrite the orginal styling

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-const 
+const bodyBackGround = document.querySelector('body');
+bodyBackGround.style.backgroundImage = "url('./Navarro-Designs_01-510x340.jpg')";
+
+const hOneHeading = document.querySelector('h1');
+hOneHeading.style.textDecoration = "underline overline";
+hOneHeading.style.border = "blue";
