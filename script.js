c659b579d5fd9e9a3b3d300d2333882a206e5542
Bogdan Kowaltchook <bogdan.kowaltchook@codeimmersives.com>
2020-02-11 10:54:48 -0500

    added some extra and figured out how to querySelect a class and id

--- script.js
+++ script.js
@@ -3,4 +3,12 @@ bodyBackGround.style.backgroundImage = "url('./Navarro-Designs_01-510x340.jpg')"
 
 const hOneHeading = document.querySelector('h1');
 hOneHeading.style.textDecoration = "underline overline";
-hOneHeading.style.border = "blue";
+hOneHeading.style.color = "blue";
+
+const topText = document.querySelector('.top-text');
+
+topText.style.color = "green";
+topText.style.float = "right";
+topText.style.fontSize = '5em';
+
+
